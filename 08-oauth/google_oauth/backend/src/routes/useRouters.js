b6b12f601adb8e289fc27cdf3dad9c71ef2f0da7
bucky0112 <bucky0112@gmail.com>
2025-05-20 18:10:14 +0800

    hidden answer

--- 08-oauth/google_oauth/backend/src/routes/useRouters.js
+++
@@ -1,8 +0,0 @@
-const express = require('express')
-const router = express.Router()
-const { googleAuth, googleAuthCallback } = require('../controllers/useController')
-
-router.get('/google', googleAuth)
-router.get('/google/callback', googleAuthCallback)
-
-module.exports = router
